9f5701cb0c87dd3bf9e518cb2bded7d2cc4c62a9
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-06-04 11:41:54 +0200

    manual changes

--- integration-tests/client/src/main/java/io/quarkiverse/cxf/client/it/CxfClientResource.java
+++ integration-tests/client/src/main/java/io/quarkiverse/cxf/client/it/CxfClientResource.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -51,7 +50,7 @@ public class CxfClientResource {
     CalculatorService myFaultyCalculator;
 
     @Inject
-    @Named("org.jboss.eap.quickstarts.wscalculator.calculator.CalculatorService")
+    @CXFClient("codeFirstClient") // name used in application.properties
     CXFClientInfo calculatorClientInfo;
 
     @Inject
